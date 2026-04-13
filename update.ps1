139bf0690a3b1ed7057dea44be2d413821865cda
whiteyasmin <whiteyasminn@hotmail.com>
2026-04-14 07:34:48 +0800

    Delete update.ps1

--- update.ps1
+++
@@ -1,4 +0,0 @@
-﻿$src = Get-Content src/strategyEngine.ts -Raw -Encoding UTF8
-$code = Get-Content replace.ts -Raw -Encoding UTF8
-$src = $src -replace '(?s)export function evaluateMispricingOpportunity\(params: MispricingEvaluationParams\): MispricingEvaluation \{.*\}', $code
-Set-Content src/strategyEngine.ts $src -Encoding UTF8
